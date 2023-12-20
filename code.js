b162c122d96fc8073d5db9d3b8a43cc34c1e9175
adiaviad <adaviad4@gmail.com>
2023-12-20 18:17:32 +0200

    formatted a json from xsl

--- code.js
+++ code.js
@@ -9,8 +9,25 @@ function getResourceData(){
         const sheet = workbook.Sheets[sheetName];
     
     
-        const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 0 });
-        console.log(jsonData);
+        const xslData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+        console.log("xsl begin",xslData);
+        jsonData={
+            "x":[],
+            "data_sets": []
+        }
+        xValues=xslData[0].slice(1);
+        //start with the second line bc the first line is the names of recources
+        for(let i =1;i<xslData.length;i++){
+            if(xslData[i].lenght<1){
+                break; //after the info about provinces there is a break and then other irrelevent data. we break to not add that data to the json
+            }
+
+            jsonData.data_sets.push({
+                "y":xslData[i].slice(1),
+                "name":xslData[i][0]
+            });
+        }         
+        console.log("xsl begin",xslData);
         return jsonData;
     })
     .catch(error => console.error('Error fetching the Excel file:', error));
