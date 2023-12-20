79ec2c15ca2ed3322edf614261d2743b6a66be89
adiaviad <adaviad4@gmail.com>
2023-12-20 10:58:32 +0200

    added a function to read the excel file

--- code.js
+++ code.js
@@ -1,16 +1,17 @@
 console.log("another file ")
-fetch('hon_planning_and_dev.xlsx')
-  .then(response => response.arrayBuffer())
-  .then(data => {
-    var workbook = XLSX.read(data, { type: 'array' });
-
-    // Assuming the first sheet is of interest
-    var sheetName = workbook.SheetNames[0];
-    var sheet = workbook.Sheets[sheetName];
-
-    // Access sheet data as JSON
-   
-    var jsonData = XLSX.utils.sheet_to_json(sheet, { header: 0 });
-    console.log(jsonData);
-  })
-  .catch(error => console.error('Error fetching the Excel file:', error));
+function getResourceData(){
+    return fetch('hon_planning_and_dev.xlsx')
+    .then(response => response.arrayBuffer())
+    .then(data => {
+        const workbook = XLSX.read(data, { type: 'array' });
+        //the third sheet is the one with "mash'ab hon"
+        const sheetName = workbook.SheetNames[2];
+        const sheet = workbook.Sheets[sheetName];
+    
+    
+        const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 0 });
+        console.log(jsonData);
+        return jsonData;
+    })
+    .catch(error => console.error('Error fetching the Excel file:', error));
+}
