33888822966558e4daa375e11bdd07ae16d80eb8
adiaviad <adaviad4@gmail.com>
2023-12-30 12:48:59 +0200

    parsed a json

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db/db');
-
+const tableName='tabl1';
 router.get('/getData', (req, res) => {
-  db.query('SELECT * FROM tabl1', (error, results) => {
+  db.query('SELECT * FROM '+tableName, (error, results) => {
     if (error) {
       console.error('Error executing query:', error);
       res.status(500).send('Internal Server Error');
@@ -15,4 +15,22 @@ router.get('/getData', (req, res) => {
   });
 });
 
+router.get('/getColumnNames', (req, res) => {
+  // const tableName = 'table1'; // Replace with your actual table name
+
+  // Query to get column names for the specified table
+  const query = `SELECT COLUMN_NAME
+                 FROM INFORMATION_SCHEMA.COLUMNS
+                 WHERE TABLE_NAME = '${tableName}'`;
+
+  db.query(query, (error, results) => {
+    if (error) {
+      console.error('Error executing query:', error);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    console.log("Column names:", results);
+    res.json(results);
+  });
+});
 module.exports = router;
