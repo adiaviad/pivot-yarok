69dac7d84bb7b35d4bc5c6f2a1031753972fee1c
adiaviad <adaviad4@gmail.com>
2024-06-14 02:02:55 +0300

    both dropdowns work

---
+++ public/js/jsonNames.js
@@ -0,0 +1,42 @@
+const resHierarchy = {
+    "resources": [
+        {
+            "value": "PlanAndDev",
+            "name": "משאב על פיתוח ותכנון",
+            "supermeasures": [
+                {
+                    name: "מדד על תיכנון",
+                    value: "plan"
+                },
+                {
+                    name: "מדד על פיתוח",
+                    value: "dev"
+                }
+            ]
+        }
+    ]
+}
+const resourceNaming = {
+    PlanAndDev: {
+        name: "משאב על פיתוח ותכנון",
+        value: "PlanAndDev",
+        plan: {
+            name: "מדד על תיכנון",
+            value: "plan"
+        },
+        dev: {
+            name: "מדד על פיתוח",
+            value: "dev"
+        }
+    },
+}
+const measureNaming = {
+    plan: {
+        name: "מדד על תיכנון",
+        value: "plan"
+    },
+    dev: {
+        name: "מדד על פיתוח",
+        value: "dev"
+    }
+}
