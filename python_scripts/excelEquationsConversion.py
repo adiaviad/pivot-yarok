e6d11eb57b018f4fc7dbfbb16a893cd2b8de0a0c
adiaviad <adaviad4@gmail.com>
2024-06-16 21:35:48 +0300

    script now handle columns like AB AC, BA

--- python_scripts/excelEquationsConversion.py
+++ python_scripts/excelEquationsConversion.py
@@ -1,8 +1,20 @@
 import re
 
+
+def excelColumnToIndex(string):
+    s = 0
+    base = ord("Z") - ord("A") + 1
+    string = string[::-1]
+    for i in range(len(string)):
+        current = ord(string[i].upper()) - ord("A") + 1
+        s += current * (base ** i)
+    return s - 2
+
+
 # example for equation: "=E7*100000/H7"
 print("example for equation:\n=E7*100000/H7")
-f=open("equations.txt",'r');
+
+f = open("equations.txt", 'r')
 for line in f.readlines():
     excel_equation = line.strip()
 
@@ -10,6 +22,7 @@ for line in f.readlines():
     excel_equation = re.sub(r'([a-zA-Z])+[0-9]+', r'\1', excel_equation)
 
     # replaces the column from excel with an array access
-    excel_equation = re.sub(r'([a-zA-Z]+)', lambda match:f'm[{ord(match.group(0).upper()) - ord("B")}]', excel_equation)
+    excel_equation = re.sub(r'([a-zA-Z]+)', lambda match: f'm[{excelColumnToIndex(match.group(0).upper())}]',
+                            excel_equation)
 
     print(excel_equation)
